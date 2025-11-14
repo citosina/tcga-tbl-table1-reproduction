bd2eeed3e0664761daaa40112af81bc9d178ddb1
Carolina Castañeda <carolinacastaneda@MacBook-Air-de-Carolina.local>
2025-11-14 13:32:41 -0600

    Move download.sh to repo root; cleanup input_data

--- input_data/download.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-gdc-client download -m gdc_manifest_maf.txt
-
-gdc-client download -m gdc_manifest_cnv_TCGA_all.txt
-
-echo "combined_study_clinical_data.tsv is already included in input_data/"
